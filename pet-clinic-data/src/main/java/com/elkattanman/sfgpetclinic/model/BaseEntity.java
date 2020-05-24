6de3d4e4c67728192651b91b2ce4e7f9f86388c3
Mustafa Khaled <asdxasd012@gmail.com>
2020-05-24 05:36:20 +0200

    adding base entity. closes #10.

---
+++ pet-clinic-data/src/main/java/com/elkattanman/sfgpetclinic/model/BaseEntity.java
@@ -0,0 +1,16 @@
+package com.elkattanman.sfgpetclinic.model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+
+    Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
--- pet-clinic-data/src/main/java/com/elkattanman/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/elkattanman/sfgpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.elkattanman.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
     private  PetType petType;
     private Owner owner;
     private LocalDate birthday;
