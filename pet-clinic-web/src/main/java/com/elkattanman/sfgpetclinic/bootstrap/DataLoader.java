49261dccfc757cbeed74d84e33c9f7a17a58569e
Mustafa Khaled <asdxasd012@gmail.com>
2020-05-31 09:18:53 +0200

    Added Owners Lst and updated controller. Closes #16

--- pet-clinic-web/src/main/java/com/elkattanman/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/elkattanman/sfgpetclinic/bootstrap/DataLoader.java
@@ -32,7 +32,7 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner1);
 
         Owner owner2=new Owner();
-        owner2.setId(1L);
+        owner2.setId(2L);
         owner2.setFirstName("Shimaa");
         owner2.setLastName("Khallaf");
 
