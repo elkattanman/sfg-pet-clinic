187b8eb98423462b5cc5b5002fb3e1bcf4ab0c44
Mustafa Khaled <asdxasd012@gmail.com>
2020-05-31 04:29:20 +0200

    adding owners page and controller. closes #13.

---
+++ pet-clinic-web/src/main/java/com/elkattanman/sfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,16 @@
+package com.elkattanman.sfgpetclinic.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/index", "index.html"})
+    public String listOwners(){
+        return "owners/index";
+    }
+
+}
