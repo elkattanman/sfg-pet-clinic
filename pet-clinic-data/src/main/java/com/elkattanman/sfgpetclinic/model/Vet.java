afb1810e3002d81f5b7ce974e4ed16166e60fcf7
Mustafa Khaled <asdxasd012@gmail.com>
2020-06-01 01:49:52 +0200

    added Speciality, added to vet , closes #27

--- pet-clinic-data/src/main/java/com/elkattanman/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/elkattanman/sfgpetclinic/model/Vet.java
@@ -1,6 +1,16 @@
 package com.elkattanman.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
+    private Set<Speciality> speciality;
 
+    public Set<Speciality> getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(Set<Speciality> speciality) {
+        this.speciality = speciality;
+    }
 }
