596b1abe22cf692b886a9dc3195b3841b3cf7eae
Mustafa Khaled <asdxasd012@gmail.com>
2020-06-01 01:04:02 +0200

    added Wroj4 plugin and config from Spring pet Clinic. Closes #24

--- pet-clinic-data/src/main/java/com/elkattanman/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/elkattanman/sfgpetclinic/services/OwnerService.java
@@ -1,9 +1,8 @@
 package com.elkattanman.sfgpetclinic.services;
 
 import com.elkattanman.sfgpetclinic.model.Owner;
-import sun.rmi.runtime.Log;
 
-import java.util.Set;
+
 
 public interface OwnerService extends CrudService<Owner , Long> {
     Owner findByLastName(String lastName);
