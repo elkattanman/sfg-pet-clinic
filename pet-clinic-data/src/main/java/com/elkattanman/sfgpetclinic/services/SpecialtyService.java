46534fc134ceab10e509e38be644332f223f9995
Mustafa Khaled <asdxasd012@gmail.com>
2020-06-01 02:44:37 +0200

    added Speciality Map impl, closes #31

---
+++ pet-clinic-data/src/main/java/com/elkattanman/sfgpetclinic/services/SpecialtyService.java
@@ -0,0 +1,7 @@
+package com.elkattanman.sfgpetclinic.services;
+
+import com.elkattanman.sfgpetclinic.model.Speciality;
+
+
+public interface SpecialtyService extends CrudService<Speciality, Long> {
+}
