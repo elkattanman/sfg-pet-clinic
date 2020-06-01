650adb8d25167af934847a1624024d43ba850eb0
Mustafa Khaled <asdxasd012@gmail.com>
2020-06-01 02:23:51 +0200

    added PetType to bootstrap, closes #32.

--- pet-clinic-web/src/main/java/com/elkattanman/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/elkattanman/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,11 +1,11 @@
 package com.elkattanman.sfgpetclinic.bootstrap;
 
 import com.elkattanman.sfgpetclinic.model.Owner;
+import com.elkattanman.sfgpetclinic.model.PetType;
 import com.elkattanman.sfgpetclinic.model.Vet;
 import com.elkattanman.sfgpetclinic.services.OwnerService;
+import com.elkattanman.sfgpetclinic.services.PetTypeService;
 import com.elkattanman.sfgpetclinic.services.VetService;
-import com.elkattanman.sfgpetclinic.services.map.OwnerServiceMap;
-import com.elkattanman.sfgpetclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -14,15 +14,25 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1=new Owner();
         owner1.setFirstName("Mustafa");
@@ -49,7 +59,6 @@ public class DataLoader implements CommandLineRunner {
         vet2.setLastName("micheal");
 
         vetService.save(vet2);
-
         System.out.println("Loading Vets...");
 
 
