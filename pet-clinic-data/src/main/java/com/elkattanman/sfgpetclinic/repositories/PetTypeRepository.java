355c9ba2429f7348e822b0e30a5151252e065b99
mustafa Khaled <asdxasd012@gmail.com>
2020-09-05 19:14:08 +0200

    added spring data jpa service for owners. closes #40

--- pet-clinic-data/src/main/java/com/elkattanman/sfgpetclinic/repositories/PetTypeRepository.java
+++ pet-clinic-data/src/main/java/com/elkattanman/sfgpetclinic/repositories/PetTypeRepository.java
@@ -3,5 +3,5 @@ package com.elkattanman.sfgpetclinic.repositories;
 import com.elkattanman.sfgpetclinic.model.PetType;
 import org.springframework.data.repository.CrudRepository;
 
-public interface petTypeRepository extends CrudRepository<PetType, Long> {
+public interface PetTypeRepository extends CrudRepository<PetType, Long> {
 }
