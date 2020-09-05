20b319b0f180c894969ce8593fd55c4b568b0bdf
mustafa Khaled <asdxasd012@gmail.com>
2020-09-05 19:28:07 +0200

    added Pet SD JPA Service closes #43

--- pet-clinic-data/src/main/java/com/elkattanman/sfgpetclinic/repositories/PetRepository.java
+++ pet-clinic-data/src/main/java/com/elkattanman/sfgpetclinic/repositories/PetRepository.java
@@ -1,6 +1,7 @@
 package com.elkattanman.sfgpetclinic.repositories;
 
+import com.elkattanman.sfgpetclinic.model.Pet;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PetRepository extends CrudRepository<PetRepository, Long> {
+public interface PetRepository extends CrudRepository<Pet, Long> {
 }
