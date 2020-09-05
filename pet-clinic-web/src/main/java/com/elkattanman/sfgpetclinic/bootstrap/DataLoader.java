4a9705a54d11d3b56b05f79bf436aaeaf82eeb6d
mustafa Khaled <asdxasd012@gmail.com>
2020-09-05 17:41:10 +0200

    fix broken links, Closes #45

--- pet-clinic-web/src/main/java/com/elkattanman/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/elkattanman/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,11 +1,9 @@
 package com.elkattanman.sfgpetclinic.bootstrap;
 
-import com.elkattanman.sfgpetclinic.model.Owner;
-import com.elkattanman.sfgpetclinic.model.Pet;
-import com.elkattanman.sfgpetclinic.model.PetType;
-import com.elkattanman.sfgpetclinic.model.Vet;
+import com.elkattanman.sfgpetclinic.model.*;
 import com.elkattanman.sfgpetclinic.services.OwnerService;
 import com.elkattanman.sfgpetclinic.services.PetTypeService;
+import com.elkattanman.sfgpetclinic.services.SpecialtyService;
 import com.elkattanman.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -18,17 +16,26 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
     private final PetTypeService petTypeService;
+    private final SpecialtyService specialtyService;
 
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
+        this.specialtyService = specialtyService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        int count = petTypeService.findAll().size();
 
+        if (count == 0 ){
+            loadData();
+        }
+    }
+
+    private void loadData() {
         PetType dog = new PetType();
         dog.setName("Dog");
         PetType savedDogPetType = petTypeService.save(dog);
@@ -36,6 +43,18 @@ public class DataLoader implements CommandLineRunner {
         PetType cat = new PetType();
         cat.setName("Cat");
         PetType savedCatPetType = petTypeService.save(cat);
+
+        Specialty radiology = new Specialty();
+        radiology.setDescription("Radiology");
+        Specialty savedRadiology = specialtyService.save(radiology);
+
+        Specialty surgery = new Specialty();
+        surgery.setDescription("Surgery");
+        Specialty savedSurgery = specialtyService.save(surgery);
+
+        Specialty dentistry = new Specialty();
+        dentistry.setDescription("dentistry");
+        Specialty savedDentistry = specialtyService.save(dentistry);
 
         Owner owner1=new Owner();
         owner1.setFirstName("Mustafa");
@@ -72,21 +91,21 @@ public class DataLoader implements CommandLineRunner {
 
         System.out.println("Loading Owners... ");
 
+
         Vet vet1 = new Vet();
-        vet1.setFirstName("sam");
+        vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
+        vet1.getSpecialities().add(savedRadiology);
 
         vetService.save(vet1);
 
-        Vet vet2=new Vet();
-        vet2.setFirstName("Jon");
-        vet2.setLastName("micheal");
+        Vet vet2 = new Vet();
+        vet2.setFirstName("Jessie");
+        vet2.setLastName("Porter");
+        vet2.getSpecialities().add(savedSurgery);
 
         vetService.save(vet2);
-        System.out.println("Loading Vets...");
 
-
-
-
+        System.out.println("Loaded Vets....");
     }
 }
--- pet-clinic-web/src/main/java/com/elkattanman/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/elkattanman/sfgpetclinic/controllers/OwnerController.java
@@ -24,4 +24,9 @@ public class OwnerController {
         return "owners/index";
     }
 
+    @RequestMapping("/find")
+    public String findOwners(){
+        return "notimplemented";
+    }
+
 }
