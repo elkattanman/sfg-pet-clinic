296320b329720ccb825307964f3e5ac3913e9660
Mustafa Khaled <asdxasd012@gmail.com>
2020-09-28 02:29:06 +0200

    add oups path. closes #58

--- pet-clinic-data/src/main/java/com/elkattanman/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/elkattanman/sfgpetclinic/repositories/OwnerRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
     Owner findByLastName(String lastName);
 
     List<Owner> findAllByLastNameLike(String lastName);
---
+++ pet-clinic-web/src/main/java/com/elkattanman/sfgpetclinic/controllers/CrashController.java
@@ -0,0 +1,16 @@
+package com.elkattanman.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+class CrashController {
+
+	@GetMapping("/oups")
+	public String triggerException() {
+		throw new RuntimeException(
+				"Expected: controller used to showcase what "
+						+ "happens when an exception is thrown");
+	}
+
+}
